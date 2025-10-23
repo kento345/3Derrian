7c50fd7270c048a2a76e3ae5c55745c067a13ff9
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2025-10-23 23:38:03 +0900

    Holeに当たった時の敵状態 HoleMoveScriptの途中

---
+++ Assets/AttackMove.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AttackMove : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/HoleMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class HoleMove : MonoBehaviour
+{
+    [SerializeField] private LayerMask holeLayer;
+
+    private bool isHole;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Hole()
+    {
+        
+    }
+}
